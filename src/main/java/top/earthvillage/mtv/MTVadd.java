777e5ba09ab373a06ad13f92dfce12381b18a87d
MinecraftEarthVillage <earthvillage.top@outlook.com>
2024-08-23 20:37:03 +0800

    修复
    修复

--- src/main/java/top/earthvillage/mtv/MTVadd.java
+++ src/main/java/top/earthvillage/mtv/MTVadd.java
@@ -1,10 +1,12 @@
 package top.earthvillage.mtv;
 
+import nl.mtvehicles.core.commands.vehiclesubs.VehicleDelete;
 import nl.mtvehicles.core.events.VehicleEnterEvent;
 import nl.mtvehicles.core.events.VehicleLeaveEvent;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.vehicle.VehicleDestroyEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.Bukkit;
@@ -18,11 +20,7 @@ public class MTVadd extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
 
-        if (!MTV载具在吗()) {
-            getServer().getConsoleSender().sendMessage(ChatColor.RED + "未检测到插件: MTVehicle, 插件卸载中（一定要两个一起用）");
-            getServer().getPluginManager().disablePlugin(this);
-            return;
-        }
+
         // 注册事件监听器
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info("事件监听器注册成功"); // 确保这行代码出现在控制台中
@@ -30,19 +28,12 @@ public class MTVadd extends JavaPlugin implements Listener {
         saveDefaultConfig();
         this.getCommand("mtvaddreload").setExecutor(this);
     }
-    //加载前置插件
-    private boolean MTV载具在吗() {
-        if (getServer().getPluginManager().getPlugin("MTVehicle") == null) {
-            return false;
-        }
-        return false;
-    }
+
 
     @EventHandler
     public void onVehicleEnter(VehicleEnterEvent event) {
         Player player = event.getPlayer();
         // 获取配置中的指令
-        String command = getConfig().getString("enter-command");
 executeCommands(player,getConfig().getStringList("上车指令"));
         getLogger().info(player.getName() + "上车了");
 
@@ -55,7 +46,6 @@ executeCommands(player,getConfig().getStringList("上车指令"));
         executeCommands(player, getConfig().getStringList("下车指令"));
         getLogger().info(player + "下车了");
     }
-
 
 
 
@@ -89,7 +79,7 @@ executeCommands(player,getConfig().getStringList("上车指令"));
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("mtvreload")) {
+        if (command.getName().equalsIgnoreCase("mtvaddreload")) {
             this.reloadConfig();
             sender.sendMessage("MTV-add 插件配置已重载！");
             return true;
